4493d51a2f05e476a248c0765e4a74ab59d96e2d
Aquib <aquib.mujtaba@live.com>
2018-07-11 00:37:17 +0530

    upto blog-list Table

---
+++ src/app/blog/Blog.ts
@@ -0,0 +1,8 @@
+export interface IBlog
+{
+    id: number,
+    authorId: number,
+    title: string,
+    text: string
+    // postDate: string
+}
--- src/app/blog/blog-list/blog-list.component.ts
+++ src/app/blog/blog-list/blog-list.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-
+import { IBlog } from '../Blog';
+import blogArray from "../blogs";
 @Component({
   selector: 'app-blog-list',
   templateUrl: './blog-list.component.html',
   styleUrls: ['./blog-list.component.css']
 })
 export class BlogListComponent implements OnInit {
-
+blogs : IBlog[];
   constructor() { }
 
   ngOnInit() {
+    this.blogs=blogArray;
   }
 
 }
